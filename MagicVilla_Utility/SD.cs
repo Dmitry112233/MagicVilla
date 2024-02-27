103e3d135572d6e4d9251c3065308c1d826d3d7f
Dmitry Starovoytov <>
2024-02-27 13:09:19 +0300

    added web project for view, copied all dto, added automapper and villa api url to appsettings

---
+++ MagicVilla_Utility/SD.cs
@@ -0,0 +1,12 @@
+﻿namespace MagicVilla_Utility;
+
+public static class Sd
+{
+    public enum ApiType
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+}
---
+++ MagicVilla_Web/Models/ApiResponse.cs
@@ -0,0 +1,11 @@
+using System.Net;
+
+namespace MagicVilla_Web.Models;
+
+public class ApiResponse
+{
+    public HttpStatusCode StatusCode { get; set; }
+    public bool IsSuccess { get; set; } = true;
+    public List<string> ErrorMessages { get; set; } = new ();
+    public object Result { get; set; }
+}
